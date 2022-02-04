160d79c3d255645ed03461e16990e9712d4cc4ae
Stanislav Buinovsky <sbuinovsky@gmail.com>
2022-02-04 13:13:48 +0300

    Initial Commit

---
+++ Contacts/ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by Станислав Буйновский on 04.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
